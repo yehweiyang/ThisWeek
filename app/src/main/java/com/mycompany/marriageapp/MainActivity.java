c410d47458ac8d8f6a1803f05e208158f41d222a
unknown <tw00182691@gmail.com>
2015-01-19 17:06:01 +0800

    不懂

--- app/src/main/java/com/mycompany/marriageapp/MainActivity.java
+++ app/src/main/java/com/mycompany/marriageapp/MainActivity.java
@@ -1,67 +1,88 @@
+
+
+
 package com.mycompany.marriageapp;
 
-
-
-        import android.support.v7.app.ActionBarActivity;
-        import android.support.v7.app.ActionBar;
-        import android.support.v4.app.Fragment;
         import android.os.Bundle;
-        import android.view.LayoutInflater;
+        import android.app.Activity;
         import android.view.Menu;
-        import android.view.MenuItem;
         import android.view.View;
-        import android.view.ViewGroup;
-        import android.os.Build;
+        import android.widget.AdapterView;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Spinner;
+        import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private EditText mEdtAge;
+    private Button mBtnOK;
+    private TextView mTxtR;
+    private Spinner mSpnSex;
+    private String msSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        mEdtAge = (EditText) findViewById(R.id.edtAge);
+        mBtnOK = (Button) findViewById(R.id.btnOK);
+        mTxtR = (TextView) findViewById(R.id.txtR);
 
+        mBtnOK.setOnClickListener(btnOKOnClick);
+
+//        mSpnSex = (Spinner) findViewById(R.id.spnSex);
+//        mSpnSex.setOnItemSelectedListener(spnSexOnItemSelected);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
+    private View.OnClickListener btnOKOnClick = new View.OnClickListener() {
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+        public void onClick(View v) {
+            int iAge = Integer.parseInt(mEdtAge.getText().toString());
 
+            String strSug = getString(R.string.result);
+            if (msSex.equals(getString(R.string.sex_male)))
+                if (iAge < 28)
+                    strSug += getString(R.string.sug_not_hurry);
+                else if (iAge > 33)
+                    strSug += getString(R.string.sug_get_married);
+                else
+                    strSug += getString(R.string.sug_find_couple);
+            else
+            if (iAge < 25)
+                strSug += getString(R.string.sug_not_hurry);
+            else if (iAge > 30)
+                strSug += getString(R.string.sug_get_married);
+            else
+                strSug += getString(R.string.sug_find_couple);
+
+            mTxtR.setText(strSug);
+        }
+    };
+
+    private AdapterView.OnItemSelectedListener spnSexOnItemSelected =
+            new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView<?> parent,
+                                           View v,
+                                           int position,
+                                           long id) {
+                    msSex = parent.getSelectedItem().toString();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            };
 }
